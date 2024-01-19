87bb3f61f484a633f9f632fe8ae0d825695ca645
carrexxii <cjbarrett101@outlook.com>
2024-01-19 13:48:48 +0200

    Add vector and bivector duality operators

--- algebra.fs
+++ algebra.fs
@@ -13,7 +13,7 @@ open System
 module PGA2D =
     let basis = [ ""; "e0"; "e1"; "e2"; "e01"; "e02"; "e12"; "e012" ]
 
-    let ( ! ) (x: float<_>) =
+    let ( !<> ) (x: float<_>) =
         float x
 
     let vecStr elems suffixes =
@@ -32,6 +32,7 @@ module PGA2D =
             | x::xs, b::bs -> coeff x + plus (x, xs, b) + loop (xs, bs)
         loop (elems, suffixes)
         |> _.Replace("+ -", "- ")
+        |> _.TrimEnd([|' '; '+'|])
         |> (fun s -> if s = "" then "0" else s)
 
     type Blade = 
@@ -53,16 +54,24 @@ module PGA2D =
               e1 = b * 1.0<e1>
               e2 = c * 1.0<e2> }
 
-        member this.list = [ !this.e0; !this.e1; !this.e2 ]
+        member this.list = [ !<>this.e0; !<>this.e1; !<>this.e2 ]
 
         member this.Item = function
-            | 0 -> !this.e0
-            | 1 -> !this.e1
-            | 2 -> !this.e2
+            | 0 -> !<>this.e0
+            | 1 -> !<>this.e1
+            | 2 -> !<>this.e2
             | _ -> raise (IndexOutOfRangeException ())
 
         override this.ToString () =
             vecStr this.list (basis |> List.skip 1 |> List.take 3)
+
+        static member ( !* ) (a: Vec) = // Poincare Dual
+            { e01 =  a.e2 * 1.0<e01/e2>
+              e02 = -a.e1 * 1.0<e02/e1>
+              e12 =  a.e0 * 1.0<e12/e0> }
+        static member ( !** ) (a: Vec) = !*a // Hodge dual
+
+        static member ( ~~ ) (a: Vec) = !*(!*a) // Inverse
 
         static member ( + ) (a: Vec, b: Vec) =
             { e0 = a.e0 + b.e0
@@ -83,6 +92,9 @@ module PGA2D =
               e12 = a.e1*b.e2 - a.e2*b.e1 }
         static member ( .^. ) (a: Vec, B: Bivec) =
             { e012 = a.e0*B.e12 - a.e1*B.e02 + a.e2*B.e01 }
+        
+        static member op_Amp (a: Vec, b: Vec) =
+            ()
 
     and Bivec =
         { e01: float<e01>
@@ -97,16 +109,24 @@ module PGA2D =
               e02 = b*1.0<e02>
               e12 = c*1.0<e12> }
 
-        member this.list = [ !this.e01; !this.e02; !this.e12 ]
+        member this.list = [ !<>this.e01; !<>this.e02; !<>this.e12 ]
 
         member this.Item = function
-            | 0 -> !this.e01
-            | 1 -> !this.e02
-            | 2 -> !this.e12
+            | 0 -> !<>this.e01
+            | 1 -> !<>this.e02
+            | 2 -> !<>this.e12
             | _ -> raise (IndexOutOfRangeException ())
 
         override this.ToString () =
             vecStr this.list (basis |> List.skip 4 |> List.take 3)
+
+        static member ( !* ) (a: Bivec): Vec = // Poincare Dual
+            { e0 = a.e12 * 1.0<e0/e12>
+              e1 = a.e02 * 1.0<e1/e02>
+              e2 = a.e01 * 1.0<e2/e01> }
+        static member ( !** ) (a: Vec) = !*a // Hodge dual
+
+        static member ( ~~ ) (a: Vec) = !*(!*a) // Inverse
 
         static member ( + ) (A: Bivec, B: Bivec) =
             { e01 = A.e01 + B.e01
@@ -129,7 +149,7 @@ module PGA2D =
             { e012 = a*1.0<e012> }
 
         member this.list =
-            [ !this.e012 ]
+            [ !<>this.e012 ]
 
         override this.ToString () =
             vecStr this.list (basis |> List.skip 7 |> List.take 1)
--- tests.fs
+++ tests.fs
@@ -10,21 +10,34 @@ let testVecs =
     let c = vec 0 1 0
     let d = vec -1 0 -1
     let e = vec -1 -2 -3
+
+    let test name case expect str =
+        testCase name <| fun () ->
+            Expect.equal case expect str
+            Expect.equal $"{case}" str str
+
     testList "Vector tests" [
+        testList "Dual" [
+            test $"a* = ({a})*" !*a (bivec 3 -2  1) "3e01 - 2e02 + e12"
+            test $"b* = ({b})*" !*b (bivec 6 -5  4) "6e01 - 5e02 + 4e12"
+            test $"c* = ({c})*" !*c (bivec 0 -1  0) "-e02"
+            test $"d* = ({d})*" !*d (bivec -1 0 -1) "-e01 - e12"
+            test $"e* = ({e})*" !*e (bivec -3 2 -1) "-3e01 + 2e02 - e12"
+        ]
         testList "Outer Product" [
             testCase $"a∧b = ({a})∧({b})" <| fun () ->
                 let case   = a .^. b
-                let expect = (bivec -3 -6 -3)
+                let expect = bivec -3 -6 -3
                 Expect.equal case expect ""
                 Expect.equal $"{case}" "-3e01 - 6e02 - 3e12" ""
             testCase $"a∧c = ({a})∧({c})" <| fun () ->
                 let case   = a .^. c
-                let expect = (bivec 1 0 -3)
+                let expect = bivec 1 0 -3
                 Expect.equal case expect ""
                 Expect.equal $"{case}" "e01 - 3e12" ""
             testCase $"a∧d = ({a})∧({d})" <| fun () ->
                 let case   = a .^. d
-                let expect = (bivec 2 2 -2)
+                let expect = bivec 2 2 -2
                 Expect.equal case expect ""
                 Expect.equal $"{case}" "2e01 + 2e02 - 2e12" ""
             testCase $"a∧e = ({a})∧({e})" <| fun () ->
